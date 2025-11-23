dce2a6ac9a0fa4852c5fa29f5576708c18807ad0
Tenzin Pelletier <t2pellet@uwaterloo.ca>
2025-11-22 21:36:20 -0500

    feat: 1.20.4 neoforge (wip)

--- src/main/java/com/t2pellet/strawgolem/neoforge/StrawgolemNeoforge.java
+++ src/main/java/com/t2pellet/strawgolem/neoforge/StrawgolemNeoforge.java
@@ -1,27 +1,36 @@
-//? if forge {
-/*package com.t2pellet.strawgolem.forge;
+package com.t2pellet.strawgolem.neoforge;
 
+import com.t2pellet.haybale.Haybale;
 import com.t2pellet.haybale.HaybaleMod;
 import com.t2pellet.haybale.client.HaybaleModClient;
-import com.t2pellet.haybale.forge.HaybaleForgeMod;
-import com.t2pellet.strawgolem.Constants;
+import com.t2pellet.haybale.neoforge.HaybaleNeoforgeMod;
 import com.t2pellet.strawgolem.StrawgolemCommon;
 import com.t2pellet.strawgolem.client.StrawgolemClient;
 import com.t2pellet.strawgolem.common.events.ContainerClickHandler;
 import com.t2pellet.strawgolem.common.events.CropGrowthHandler;
 import com.t2pellet.strawgolem.common.util.container.ContainerUtil;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.entity.player.PlayerInteractEvent;
-import net.minecraftforge.event.level.BlockEvent;
-import net.minecraftforge.fml.common.Mod;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
+import net.neoforged.neoforge.event.level.BlockEvent;
 
 import java.util.function.Consumer;
 
+@Mod(Haybale.MODID)
+@HaybaleMod.IMod(Haybale.MODID)
+public class StrawgolemNeoforge extends HaybaleNeoforgeMod {
 
-@Mod(Constants.MOD_ID)
-@HaybaleMod.IMod(Constants.MOD_ID)
-public class StrawgolemForge extends HaybaleForgeMod {
+    private static StrawgolemNeoforge instance;
+
+    public static StrawgolemNeoforge getInstance() {
+        return instance;
+    }
+
+    public StrawgolemNeoforge(IEventBus modBus) {
+        super(modBus);
+    }
 
     @Override
     protected HaybaleMod getCommonMod() {
@@ -34,17 +43,17 @@ public class StrawgolemForge extends HaybaleForgeMod {
     }
 
     @Override
-    protected void registerEvents() {
-        MinecraftForge.EVENT_BUS.addListener((Consumer<BlockEvent.CropGrowEvent>) event -> {
+    protected void registerEvents(IEventBus modBus) {
+        NeoForge.EVENT_BUS.addListener((Consumer<BlockEvent.CropGrowEvent>) event -> {
             if (event.getLevel() instanceof ServerLevel serverLevel) {
                 CropGrowthHandler.onCropGrowth(serverLevel, event.getPos());
             }
         });
-        MinecraftForge.EVENT_BUS.addListener((Consumer<PlayerInteractEvent.RightClickBlock>) event -> {
+        NeoForge.EVENT_BUS.addListener((Consumer<PlayerInteractEvent.RightClickBlock>) event -> {
             if (ContainerUtil.isContainer(event.getLevel(), event.getPos())) {
                 ContainerClickHandler.onContainerClicked(event.getEntity(), event.getPos());
             }
         });
+        super.registerEvents(modBus);
     }
 }
-*///?}
